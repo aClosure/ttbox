cdd48219090704329d0069220e51d0eab10689da
aClosure <31314973@qq.com>
2026-07-26 11:16:24 +0800

    Average example resource

--- c_initial/Average.c
+++ c_initial/Average.c
@@ -3,9 +3,11 @@
 int main() 
 {
     int a = 0, b = 0;
-    printf("Hello BUPT 2026!!!\n");
+    double c = 0;
+    printf("Please enter two integers: a =  b =   \n");
     scanf("%d %d", &a, &b);
-    printf("%d 除以 %d equals to %f\n",a,b, (float)a/b);
+    c = (double)(a + b) / 2;
+    printf("%d 除以 %d equals to %f\n", a, b, c);
 
     return 0;
 }
