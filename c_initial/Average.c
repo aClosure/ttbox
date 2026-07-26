528f6524797b30067ff503b1342e417f1d27659c
aClosure <31314973@qq.com>
2026-07-26 11:12:39 +0800

    Average example resource

---
+++ c_initial/Average.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 0, b = 0;
+    printf("Hello BUPT 2026!!!\n");
+    scanf("%d %d", &a, &b);
+    printf("%d 除以 %d equals to %f\n",a,b, (float)a/b);
+
+    return 0;
+}
